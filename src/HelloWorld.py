0bf144d864bae3919266ff8a6cbc73d74214a55d
icekiller <biewutaai@hotmail.com>
2014-01-12 00:12:53 +0800

    c_int

--- src/HelloWorld.py
+++ src/HelloWorld.py
@@ -1,1 +1,16 @@
+from ctypes import *
+msvcrt = cdll.msvcrt
+message_string = "HelloWorld!\n"
+msvcrt.printf("Testing:%s",message_string)
 
+class barley_amount(Union):
+    _fields_ = [
+               ("barley_long",c_long),
+               ("barley_int",c_int),
+               ("barley_char",c_char * 8),
+               ]
+value = raw_input("put some num:")
+myBarley = barley_amount(int(value))
+print myBarley.barley_long
+print myBarley.barley_int
+print myBarley.barley_char
